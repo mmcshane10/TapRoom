7f2b2441e4acc34054b3dad9281b84bc0bc2b1b7
Mike <mmcshane10@gmail.com>
2019-11-15 14:09:17 -0800

    form and button styling

--- src/components/NewBeerForm.jsx
+++ src/components/NewBeerForm.jsx
@@ -2,31 +2,48 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 
 function NewBeerForm() {
+  var formStyle = {
+    marginTop: '30px',
+    marginLeft: '20%',
+    marginRight: '20%',
+  }
+  var buttonStyle = {
+    backgroundColor: '#110133'
+  }
   return (
-    <div>
-      <form>
+    <div className='container'>
+      <form style={formStyle}>
+        <div className='input-field col s12'>
+          <input
+            type='text'
+            id='name'
+            placeholder='Beer Name' />
+        </div>
+        <div className='input-field col s12'>
+          <input
+            type='text'
+            id='brand'
+            placeholder='Brewery' />
+        </div>
+        <div className='input-field col s12'>
+          <input
+            type='text'
+            id='abv'
+            placeholder='ABV%' />
+        </div>
+        <div className='input-field col s12'>
+          <input
+            type='text'
+            id='price'
+            placeholder='Price' />
+        </div >
+        <div className='form-control'></div>
         <input
-          type='text'
-          id='name'
-          placeholder='Beer Name' />
-        <input
-          type='text'
-          id='brand'
-          placeholder='Brewery' />
-        <input
-          type='text'
-          id='abv'
-          placeholder='ABV%' />
-        <input
-          type='text'
-          id='price'
-          placeholder='Price' />
-        <input 
           type='hidden'
           id='custId'
           value='124' />
-        
-        <button type='submit'>Help!</button>
+
+        <button style={buttonStyle} type='submit' className='btn-floating btn-large waves-effect waves-light'><i className="material-icons">add</i></button>
       </form>
     </div>
   );
